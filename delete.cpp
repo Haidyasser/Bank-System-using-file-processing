816f6f0c4df6664528fe331cd56bf31ca91231cd
Haidyasser <haidyasser3@gmaail.com>
2023-04-29 18:40:56 +0300

    handle first deletion bug

--- delete.cpp
+++ delete.cpp
@@ -1,7 +1,9 @@
 #include "main.h"
-using namespace std;
 
-// delete ACOUNT 
+/**
+ * @brief This function creates a new account
+ * @return void
+ */
 void account :: delete_account()
 {
     account d;
@@ -12,19 +14,21 @@ void account :: delete_account()
 
     cout << "Enter the name you want to delete the account for ";
     cin >> str;
-    searchForName(str);
+    int found = searchForName(str);
+    if(found == 0)
+        return;
     cout << "Enter the id you want to delete the account for ";
     cin >> id;
     if(myfile.is_open())
     {
-        while(!myfile.eof())
+        while (myfile.read((char *)&d, sizeof(d)))
         {
-            myfile.read((char *)&d,sizeof(d));
-            if(d.id!=id)
+            if (d.id != id)
             {
-                temp.write((char *)&d,sizeof(d));
+                temp.write((char *)&d, sizeof(d));
             }
         }
+        
         myfile.close();
         temp.close();
         remove("project.txt");
