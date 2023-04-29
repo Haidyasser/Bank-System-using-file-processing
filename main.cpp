8c64d15a5580926b55514cea92aab78f8f4f42c0
Haidyasser <haidyasser3@gmaail.com>
2023-04-29 18:39:56 +0300

    update main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include "main.h"
-using namespace std;
 
-int main()
-{
-    account a;
+/**
+ * main function - entry point
+ * @return 0 on success
+ */
+int main() {
+account a;
     char ch;
+    int n ; 
     do{  
         cout<<"*****************************Bank Management System*****************************\n";
         cout<<"MAIN MENU:"<<"\n";
         cout<<"1)  NEW ACCOUNT "<<"\n";
-        //cout<<"2)  DEPOSIT AMOUNT"<<"\n";
-        //cout<<"3)  WITHDRAW AMOUNT"<<"\n";
+        cout<<"2)  DEPOSIT AMOUNT"<<"\n";
+        cout<<"3)  WITHDRAW AMOUNT"<<"\n";
         cout<<"4)  MODIFY ACCOUNT "<<"\n";
         cout<<"5)  DELETE ACCOUNT "<<"\n";
         cout<<"6)  Search for an acount "<<"\n";
@@ -24,30 +27,33 @@ int main()
             case '1':
                 a.create_account();
                 break;
-
+ 
             case '2':
-                //cout<<"Enter Your id you want to deposite\n";
-                a.deposit(1);
+                cout<<"Enter Your id to deposite\n";
+                cin>>n;
+                a.deposit_withdraw(n,1);
                 break;
-
+ 
             case '3':
-                // write function
-                cout<<"Enter amount you want to withdraw : ";
+                cout<<"Enter Your id to withdraw\n";
+                cin>>n;
+                a.deposit_withdraw(n,2);
+ 
                 break;
-
+ 
             case '4':
                 // write function
                 a.modify_account();
                 break;  
-
+ 
             case '5':
                 // write function
                 a.delete_account();
                 break;
-
+ 
             case '6':
                 // write function
-		        char name [35];
+                char name [35];
                 cout<<"Enter the name you want to search for:\n";
                 cin>>name;
                 cout<<"ID  Name  balance  age\n";
@@ -55,19 +61,19 @@ int main()
                 a.search(name);
                 cout<<"------------------------\n";
                 break;
-
+ 
             case '7':
                 // write function
                 a.display_accounts();
                 break;  
-
+ 
             case '8':
                 cout<<"Thanks for using Our Bank Management System\n";
                 break;  
-
+ 
             default:
                 break;
-
+ 
         }
         if(ch != '8'){
             char c;
@@ -76,8 +82,8 @@ int main()
             if(c == 'n' || c == 'N')
                 ch = '8';
         }
-
+ 
     }while(ch !=  '8');
-
+ 
     return 0;
 }
